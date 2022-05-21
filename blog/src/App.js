a4a93f40f7a41759202976e0b760ee26a2db7619
Shimsim <timesea96@naver.com>
2022-05-21 10:46:13 +0000

    creat, delete 추가

--- blog/src/App.js
+++ blog/src/App.js
@@ -11,6 +11,7 @@ function App() {
 	let [따봉, 따봉변경] = useState([0,0,0]);
 	let [modal, setModal] = useState(false);
 	let [title, setTitle] = useState(0);
+	let [입력값, 입력값변경] = useState('');
 
 
   return (
@@ -34,20 +35,39 @@ function App() {
 			
 			
 			{
-			글제목.map(function(a, i){
-				return (
-				<div className='list' key={i}>
-					<h4 onClick={()=>{setModal(true); setTitle(i)}}> { 글제목[i] }
-						<span onClick={()=>{
-							let copy = [...따봉]
-							copy[i] = copy[i] + 1
-							따봉변경(copy)
-					}}>👍</span> {따봉[i]}
-				</h4>
-					<p>5월 11일 발행 </p>
-				</div>)
-					})
+				글제목.map(function(a, i){
+					return (
+					<div className='list' key={i}>
+						<h4 onClick={()=>{setModal(true); setTitle(i)}}> { 글제목[i] }
+							<span onClick={()=>{
+								let copy = [...따봉]
+								copy[i] = copy[i] + 1
+								따봉변경(copy)
+						}}>👍</span> {따봉[i]}
+					</h4>
+						
+						<p>5월 11일 발행 </p>
+						<button onClick={()=> {
+									let copy = [...글제목];
+									copy.splice(i,1);
+									글변경(copy);
+								}}>삭제</button>
+					</div>
+					)
+				})
 			}	
+			
+			<input onChange={(e)=>{
+					입력값변경(e.target.value);
+				}}/>
+			
+			<button onClick={()=> {
+					let copy = [...글제목];
+					copy.push(입력값);
+					글변경(copy);
+				}}>글발행</button>
+			
+			
 			
 			{
 				modal == true ? <Modal title={title} 글변경={글변경} 글제목={글제목}/> : null
