7022804806a13747f1eadbcbf5134a749b81ffba
Shimsim <timesea96@naver.com>
2022-05-24 13:56:18 +0000

    responsive img

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+/* eslint-disable */
+
 import './App.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<Navbar bg="dark" variant="dark">
+				<Container>
+				<Navbar.Brand href="#home">ShowShop</Navbar.Brand>
+				<Nav className="me-auto">
+					<Nav.Link href="#home">Home</Nav.Link>
+					<Nav.Link href="#features">Cart</Nav.Link>
+				</Nav>
+				</Container>
+			</Navbar>
+			
+			<div className="main-bg"></div>
+			
+			<div className="container">
+				<div className="row">
+					<div className="col-md-4">
+						<img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+						<h4>상품명</h4>
+						<p>상품설명</p>
+					</div>
+					<div className="col-md-4">
+						<img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+						<h4>상품명</h4>
+						<p>상품설명</p>
+					</div>
+					<div className="col-md-4">
+						<img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+						<h4>상품명</h4>
+						<p>상품설명</p>
+					</div>
+				</div>
+			</div>
+			
     </div>
   );
 }
