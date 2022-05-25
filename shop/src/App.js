ec7e5b8b673858ee632748313684e88b93a21feb
Shimsim <timesea96@naver.com>
2022-05-25 11:18:39 +0000

    products add

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,14 @@
 /* eslint-disable */
-
+import { useState } from "react";
 import './App.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import data from './data.js';
 
 
 function App() {
+	
+	let [shoes] = useState(data);
+	
   return (
     <div className="App">
 			<Navbar bg="dark" variant="dark">
@@ -23,18 +27,18 @@ function App() {
 				<div className="row">
 					<div className="col-md-4">
 						<img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-						<h4>상품명</h4>
-						<p>상품설명</p>
+						<h4>{shoes[0].title}</h4>
+						<p>{shoes[0].price}</p>
 					</div>
 					<div className="col-md-4">
 						<img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-						<h4>상품명</h4>
-						<p>상품설명</p>
+						<h4>{shoes[1].title}</h4>
+						<p>{shoes[1].price}</p>
 					</div>
 					<div className="col-md-4">
 						<img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-						<h4>상품명</h4>
-						<p>상품설명</p>
+						<h4>{shoes[2].title}</h4>
+						<p>{shoes[2].title}</p>
 					</div>
 				</div>
 			</div>
