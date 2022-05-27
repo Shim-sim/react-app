4607762f6fd759eaa31e22294f06f798c1a1b827
Shimsim <timesea96@naver.com>
2022-05-27 14:15:00 +0000

    styled-components add

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -1,14 +1,35 @@
-function Detail() {
+/* eslint-disable */
+import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+let RedBtn = styled.button`
+	background : ${ props => props.bg };
+	color: blue;
+	padding: 10px;
+	border: none;
+`
+
+let	Box = styled.div`
+	background : grey;
+	padding: 20px;
+`
+
+function Detail(props) {
+	
+	let {id} = useParams();
+	let 찾은상품 = props.shoes.find((x)=> x.id == id)
+	
 	return(
 		<div className="container">
+			<RedBtn bg="green">그린색2버튼</RedBtn>
 			<div className="row">
 				<div className="col-md-6">
 					<img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
 				</div>
 				<div className="col-md-6">
-					<h4 className="pt-5">상품명</h4>
-					<p>상품설명</p>
-					<p>120000원</p>
+					<h4 className="pt-5">{찾은상품.title}</h4>
+					<p>{찾은상품.content}</p>
+					<p>{찾은상품.price}</p>
 					<button className="btn btn-danger">주문하기</button> 
 				</div>
 			</div>
